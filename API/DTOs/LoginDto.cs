68389c8b369d5af2c42cda411f767898df184f2c
Stuart Murray <stuart.murray.dev@outlook.com>
2024-08-20 17:27:01 +0100

    checkpoint simple auth

---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class LoginDto
+{
+    [Required]
+    public required string Username { get; set; }
+
+    [Required]
+    public required string Password { get; set; }
+
+}

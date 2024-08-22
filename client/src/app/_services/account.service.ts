508ae84b98c5f4eb6905648e26dbcbb21405b611
Stuart Murray <stuart.murray.dev@outlook.com>
2024-08-22 17:23:33 +0100

    Login / register

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -18,6 +18,19 @@ export class AccountService {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUser.set(user);
         }
+        return user;
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
       })
     );
   }

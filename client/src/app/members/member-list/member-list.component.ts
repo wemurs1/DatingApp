8df968d081dacaa598ab9a674ecfa2a69b48f65c
Stuart Murray <stuart.murray.dev@outlook.com>
2024-08-29 18:20:39 +0100

    add member service checkpoint

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
   imports: [],
   templateUrl: './member-list.component.html',
-  styleUrl: './member-list.component.css'
+  styleUrl: './member-list.component.css',
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
+  private membersService = inject(MembersService);
+  members: Member[] = [];
 
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.membersService.getMembers().subscribe({
+      next: (members) => (this.members = members),
+    });
+  }
 }

0303c9e23e2b0cbca12c78cfe6fab0f25a13313f
Stuart Murray <stuart.murray.dev@outlook.com>
2024-08-30 17:50:35 +0100

    member cards

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
@@ -9,28 +9,17 @@ import { AccountService } from './account.service';
 })
 export class MembersService {
   private http = inject(HttpClient);
-  private accountService = inject(AccountService);
   baseUrl = environment.apiUrl;
 
   getMembers() {
     return this.http.get<Member[]>(
       this.baseUrl + 'users',
-      this.getHttpOptions()
     );
   }
 
   getMember(username: string) {
     return this.http.get<Member>(
       this.baseUrl + 'users/' + username,
-      this.getHttpOptions()
     );
-  }
-
-  getHttpOptions() {
-    return {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${this.accountService.currentUser()?.token}`,
-      }),
-    };
   }
 }

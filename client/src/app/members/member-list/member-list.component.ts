8b2adff6918dad48b5b10c9553961395a89d6a46
Stuart Murray <stuart.murray.dev@outlook.com>
2024-09-02 15:23:13 +0100

    cache members

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
-import { MemberCardComponent } from "../member-card/member-card.component";
+import { MemberCardComponent } from '../member-card/member-card.component';
 
 @Component({
   selector: 'app-member-list',
@@ -11,16 +11,13 @@ import { MemberCardComponent } from "../member-card/member-card.component";
   styleUrl: './member-list.component.css',
 })
 export class MemberListComponent implements OnInit {
-  private membersService = inject(MembersService);
-  members: Member[] = [];
+  membersService = inject(MembersService);
 
   ngOnInit(): void {
-    this.loadMembers();
+    if (this.membersService.members().length === 0) this.loadMembers();
   }
 
   loadMembers() {
-    this.membersService.getMembers().subscribe({
-      next: (members) => (this.members = members),
-    });
+    this.membersService.getMembers();
   }
 }

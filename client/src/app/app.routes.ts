2958bf57cc94957280fde7f5968af06e15207b52
Stuart Murray <stuart.murray.dev@outlook.com>
2024-09-16 12:29:08 +0100

    admin and roles - checkpoint 2

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -11,6 +11,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { memberDetailedResolver } from './_resolvers/member-detailed.resolver';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { adminGuard } from './_guards/admin.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -32,6 +34,11 @@ export const routes: Routes = [
       },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
+      {
+        path: 'admin',
+        component: AdminPanelComponent,
+        canActivate: [adminGuard],
+      },
     ],
   },
   { path: 'errors', component: TestErrorsComponent },

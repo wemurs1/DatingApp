be174daf8cb2e04312e097eee73e8f77f66b5ba5
Stuart Murray <stuart.murray.dev@outlook.com>
2024-09-23 19:20:43 +0100

    checkpoint angular build in wwwroot

--- API/Program.cs
+++ API/Program.cs
@@ -25,6 +25,9 @@ app.UseCors(x => x.AllowAnyHeader()
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
